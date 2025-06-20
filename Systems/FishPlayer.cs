bd07bd8b23f4a77113a0bee248847940a74bc84d
Pearlie <bugrapearls@gmail.com>
2025-06-20 20:57:50 +0300

    Added base fields.

--- Systems/FishPlayer.cs
+++ Systems/FishPlayer.cs
@@ -7,8 +7,28 @@ using Terraria.ModLoader;
 
 namespace Ichthyology.Systems
 {
+    /// <summary>
+    /// ModPlayer class that contains fields this mod uses.
+    /// </summary>
     public class FishPlayer : ModPlayer
     {
+        //sc = Sea Creature = Enemies caught by reeling in.
 
+        /// <summary>
+        /// Chance to catch Sea Creatures.
+        /// </summary>
+        public float scChance = 0.1f;
+        /// <summary>
+        /// Bonus chance to not use bait.
+        /// </summary>
+        public float baitReserveChance = 0;
+        /// <summary>
+        /// Bonus damage dealt to Sea Creatures.
+        /// </summary>
+        public float scBonusDamage = 1;
+        /// <summary>
+        /// Damage reduction towards Sea Creatures.
+        /// </summary>
+        public float scDamageResist = 1;
     }
 }
