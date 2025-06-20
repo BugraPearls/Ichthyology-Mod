ab522469d7a9f141bdae0b5462edd0c329ef7891
Pearlie <bugrapearls@gmail.com>
2025-06-20 21:56:47 +0300

    Implemented ResetEffects and began implementation of Display accessory.

--- Systems/FishPlayer.cs
+++ Systems/FishPlayer.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Ichthyology.Systems
@@ -13,15 +16,12 @@ namespace Ichthyology.Systems
     /// </summary>
     public class FishPlayer : ModPlayer
     {
+        //sc = Sea Creature = Enemies caught by reeling in.
+
         /// <summary>
         /// Invoked in SeaCreature.cs, OnSpawn hook.
         /// </summary>
         public static Action<NPC, Player> OnSeaCreatureCaught;
-        //sc = Sea Creature = Enemies caught by reeling in.
-        public override void Load()
-        {
-            
-        }
         /// <summary>
         /// Chance to catch Sea Creatures.
         /// </summary>
@@ -38,5 +38,23 @@ namespace Ichthyology.Systems
         /// Damage reduction towards Sea Creatures.
         /// </summary>
         public float scDamageResist = 1;
+
+        public bool displaySCC;
+        public override void ResetEffects()
+        {
+            scChance = 0.1f;
+            baitReserveChance = 0;
+            scBonusDamage = 1;
+            scDamageResist = 1;
+        }
+        public override void RefreshInfoAccessoriesFromTeamPlayers(Player otherPlayer)
+        {
+            if (otherPlayer.IchthologyPlayer().displaySCC)
+                displaySCC = true;
+        }
+        public override void ResetInfoAccessories()
+        {
+            displaySCC = false;
+        }
     }
 }
