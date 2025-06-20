fe37fc87a07c85ee0fbe9f86d58e654f36b5cefd
Pearlie <bugrapearls@gmail.com>
2025-06-20 22:39:15 +0300

    Base SCC Display item classes & placeholders

---
+++ Displays/SCCDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace Ichthyology.Displays
+{
+    public class SCCDisplay : InfoDisplay
+    {
+        public static LocalizedText CurrentSCC { get; private set; }
+        //% chance to fish up an enemy
+        public override void SetStaticDefaults()
+        {
+            CurrentSCC = this.GetLocalization("CurrentSCC");
+        }
+    }
+}
---
+++ Items/SCCDisplayItem.cs
@@ -0,0 +1,24 @@
+﻿using Ichthyology.Systems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Ichthyology.Items
+{
+    public class SCCDisplayItem : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.FishermansGuide);
+        }
+        public override void UpdateInfoAccessory(Player player)
+        {
+            player.IchthologyPlayer().displaySCC = true;
+        }
+    }
+}
