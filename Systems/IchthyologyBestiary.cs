9658689f6be238354bb62b46e3ed0c5b44a30544
BugraPearls <bugrapearls@gmail.com>
2025-06-21 13:11:23 +0300

    Implemented base Bestiary

---
+++ Systems/IchthyologyBestiary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace Ichthyology.Systems
+{
+    public class IchthyologyBestiary : ModPlayer
+    {
+        public List<int> KilledSeaCreatures = new(FishIDSets.AllSC.Length);
+        public void AddToList(int IdOfSC)
+        {
+            if (KilledSeaCreatures.Contains(IdOfSC) == false)
+            {
+                KilledSeaCreatures.Add(IdOfSC);
+            }
+        }
+    }
+}
