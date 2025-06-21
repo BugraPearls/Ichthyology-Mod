ac0c94ad4169cf193c2028258b135b33f6a9a474
BugraPearls <bugrapearls@gmail.com>
2025-06-21 13:10:46 +0300

    Added debug command

---
+++ Systems/DebugCommands.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Ichthyology.Systems
+{
+    public class DebugCommands : ModCommand
+    {
+        public override CommandType Type => CommandType.Chat;
+        public override string Command => "sc";
+        public override void Action(CommandCaller caller, string input, string[] args)
+        {
+            foreach (int item in caller.Player.GetModPlayer<IchthyologyBestiary>().KilledSeaCreatures)
+            {
+                Main.NewText(ContentSamples.NpcPersistentIdsByNetIds[item]);
+            }
+        }
+    }
+}
