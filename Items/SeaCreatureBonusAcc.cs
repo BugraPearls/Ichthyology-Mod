d4c2c3405c3a9ce0dc78dae01173cd5c5b980560
BugraPearls <bugrapearls@gmail.com>
2025-06-22 14:24:39 +0300

    Test values & test acc

--- Items/SeaCreatureBonusAcc.cs
+++ Items/SeaCreatureBonusAcc.cs
@@ -5,24 +5,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
-using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Ichthyology.Items
 {
-    public class IchthyologicalSonar : ModItem
+    public class SeaCreatureBonusAcc : ModItem
     {
         public override void SetDefaults()
         {
-            Item.CloneDefaults(ItemID.FishermansGuide);
-        }
-        public override void UpdateInfoAccessory(Player player)
-        {
-            player.IchthyologyPlayer().displaySCC = true;
+            Item.DefaultToAccessory();
         }
         public override void UpdateEquip(Player player)
         {
-            player.IchthyologyPlayer().scChance += 0.95f;
+            player.IchthyologyPlayer().scBonusDamage += 2f;
+            player.IchthyologyPlayer().scDamageResist += 0.8f;
         }
     }
 }
