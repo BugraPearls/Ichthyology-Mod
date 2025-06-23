fd4579a120bcb26c7535023f7304a770f8b7816f
sskToji <kevinpeters05@outlook.com>
2025-06-23 19:07:59 +0200

    added sprites

---
+++ Projectiles/SlimeBobber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Ichthyology.Projectiles
+{
+    public class SlimeBobber : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.CloneDefaults(ProjectileID.BobberWooden);
+        }
+    }
+}
